bb700843167c5480fbebc73d67270928702d6b1e
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-03-19 01:50:09 +0300

    models.py: create a few sketch models
    For a first form to create a test.

--- hyiptest/models.py
+++ hyiptest/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    """
+    A single question.
+    """
+
+    text = models.CharField(max_length=100, help_text="The question itself")
+    description = models.CharField(
+        max_length=200,
+        blank=True,  # null=True not needed because we have empty string
+        help_text="Clarify the question",
+    )
+
+
+class Answer(models.Model):
+    """
+    An answer to a specific question.
+    """
+
+    text = models.CharField(max_length=100, help_text="The answer itself")
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        help_text="Clarify the meaning in context of the question",
+    )
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, help_text="The question you are answering"
+    )
+
+    bad_absolute = models.BooleanField(
+        # Fail by default to reduce the theoretical damage
+        # of "forgot to set the flag".
+        default=True,  # pyright: ignore[reportArgumentType]
+        help_text="If chosen by user, flags the site as fraud",
+    )
+
+
+class QGroup(models.Model):
+    """
+    A group of questions.
+    """
+
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        help_text=("Label for the group"),
+    )
+    description = models.CharField(
+        max_length=200, blank=True, help_text="Meaning of the group's name"
+    )
+    questions = models.ManyToManyField(
+        Question, help_text="That are part of this group"
+    )
