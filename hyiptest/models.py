343f0947f511e0f01d9fb0e0f468faedcacb6e83
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-03-20 22:21:38 +0300

    models.py: make help_text translatable
    To be used in the future.

--- hyiptest/models.py
+++ hyiptest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -7,11 +8,11 @@ class Question(models.Model):
     A single question.
     """
 
-    text = models.CharField(max_length=100, help_text="The question itself")
+    text = models.CharField(max_length=100, help_text=_("The question itself"))
     description = models.CharField(
         max_length=200,
         blank=True,  # null=True not needed because we have empty string
-        help_text="Clarify the question",
+        help_text=_("Clarify the question"),
     )
 
     def __str__(self):
@@ -23,21 +24,25 @@ class Answer(models.Model):
     An answer to a specific question.
     """
 
-    text = models.CharField(max_length=100, help_text="The answer itself")
+    text = models.CharField(max_length=100, help_text=_("The answer itself"))
     description = models.CharField(
         max_length=200,
         blank=True,
-        help_text="Clarify the meaning in context of the question",
+        help_text=_("Clarify the meaning in context of the question"),
     )
     question = models.ForeignKey(
-        Question, on_delete=models.CASCADE, help_text="The question you are answering"
+        Question,
+        on_delete=models.CASCADE,
+        help_text=_("The question you are answering"),
     )
 
     bad_absolute = models.BooleanField(
         # Fail by default to reduce the theoretical damage
         # of "forgot to set the flag".
         default=True,  # pyright: ignore[reportArgumentType]
-        help_text="If chosen by user, flags the site as fraud",
+        help_text=_(  # pyright: ignore[reportArgumentType]
+            "If chosen by user, flags the site as fraud"
+        ),
     )
 
     def __str__(self):
@@ -52,13 +57,13 @@ class QGroup(models.Model):
     name = models.CharField(
         max_length=50,
         unique=True,
-        help_text=("Label for the group"),
+        help_text=_("Label for the group"),
     )
     description = models.CharField(
-        max_length=200, blank=True, help_text="Meaning of the group's name"
+        max_length=200, blank=True, help_text=_("Meaning of the group's name")
     )
     questions = models.ManyToManyField(
-        Question, help_text="That are part of this group"
+        Question, help_text=_("That are part of this group")
     )
 
     def __str__(self):
