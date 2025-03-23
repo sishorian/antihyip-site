55981dbc2fc53a85ae8a39063b2663957a1b6952
Roman Krotov <152505489+sishorian@users.noreply.github.com>
2025-03-23 17:57:21 +0300

    Add script to add the list of bad sites
    To the database.

---
+++ add_badsites.py
@@ -0,0 +1,224 @@
+# Temporary script to create BadSites for the test.
+from hyiptest.models import BadSite
+
+BadSite.objects.create(
+    name="Суперкопилка", domain="superkopilka.com", bad_type="инвестиции"
+)
+
+BadSite.objects.create(
+    name="Prizm", domain="prizm.club", bad_type="криптовалюта с парамайнингом"
+)
+
+BadSite.objects.create(name="Roy Club", domain="roy.cash", bad_type="хайп")
+
+BadSite.objects.create(
+    name="River Coins", domain="river-coins.com", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Success Factory", domain="uniteam.top", bad_type="хайп, финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Hermes Management", domain="hermes-ltd.com", bad_type="хайп"
+)
+
+BadSite.objects.create(name="Kryptex", domain="kryptex.org", bad_type="хайп")
+
+BadSite.objects.create(
+    name="Finiko", domain="finiko.ru", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="B2b Jewelry", domain="b2b.jewelry", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(name="Profit Boom", domain="profit-boom.pro", bad_type="хайп")
+
+BadSite.objects.create(
+    name="Amir Capital", domain="amir.capital", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Crowd1", domain="crowd1.com", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="FTC Vin", domain="ftc.vin", bad_type="хайп, ставки на спорт, криптовалюты"
+)
+
+BadSite.objects.create(
+    name="Antares", domain="antares.trade", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Клуб Кент",
+    domain="kent.club",
+    bad_type="смесь хайпа, фп и рекламной площадки",
+)
+
+BadSite.objects.create(
+    name="Pi Network",
+    domain="minepi.com",
+    bad_type="криптовалюта, майнинг от активности",
+)
+
+BadSite.objects.create(
+    name="Forsage.io",
+    domain="forsage.io",
+    bad_type="финансовая пирамида, смарт контракт ETH, TRON",
+)
+
+BadSite.objects.create(
+    name="QubitTech", domain="qubittech.ai", bad_type="хайп, торговые роботы"
+)
+
+BadSite.objects.create(
+    name="Bitbon", domain="bitbon.space", bad_type="криптовалюта Bitbon"
+)
+
+BadSite.objects.create(
+    name="Befree-Biz", domain="befree-biz.com", bad_type="матричная финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Bin X System Pro",
+    domain="bin-x-system.pro",
+    bad_type="классическая финансовая пирамида",
+)
+
+BadSite.objects.create(name="CrowdWiz", domain="crowdwiz.biz", bad_type="криптовалюта")
+
+BadSite.objects.create(name="ГК ТИК", domain="ti.capital", bad_type="инвестиции")
+
+BadSite.objects.create(name="Sax Invest", domain="saxinvest.net", bad_type="инвестиции")
+
+BadSite.objects.create(
+    name="Artery Network",
+    domain="artery.network",
+    bad_type="собственная криптовалюта",
+)
+
+BadSite.objects.create(
+    name="Uranium Cash", domain="uranium.cash", bad_type="инвестиции"
+)
+
+BadSite.objects.create(
+    name="Market Bot", domain="ai.marketing", bad_type="инвестиции в MarketBot"
+)
+
+BadSite.objects.create(
+    name="Avalon Ltd", domain="avalon.ltd", bad_type="инвестиции в стартапы"
+)
+
+BadSite.objects.create(
+    name="Profitbot", domain="profit-bot.com", bad_type="инвестиции в свою криптовалюту"
+)
+
+BadSite.objects.create(
+    name="RobPay", domain="ex.robpay.org", bad_type="платёжная система"
+)
+
+BadSite.objects.create(
+    name="PlatinCoin", domain="platincoin.com", bad_type="криптовалюта PlatinCoin"
+)
+
+BadSite.objects.create(
+    name="Неработа", domain="ne-rabota.com", bad_type="игра, матрица и тренинг центр"
+)
+
+BadSite.objects.create(name="FrendeX", domain="frendex.info", bad_type="инвестиции")
+
+BadSite.objects.create(
+    name="Symbios Club", domain="symbios.club", bad_type="торги по банкротству"
+)
+
+BadSite.objects.create(
+    name="Teqra", domain="teqra.com", bad_type="трейдинг криптовалютами и акциями"
+)
+
+BadSite.objects.create(name="TC Group", domain="tcgroup.ai", bad_type="инвестиции")
+
+BadSite.objects.create(
+    name="Массмайнинг", domain="massmining.pro", bad_type="инвестиции в майнинг"
+)
+
+BadSite.objects.create(
+    name="Tothemars Finance",
+    domain="tothemars.finance",
+    bad_type="инвестиции в криптовалюту",
+)
+
+BadSite.objects.create(
+    name="Криптовалюта UMI", domain="roy.club", bad_type="инвестиции в криптовалюту"
+)
+
+BadSite.objects.create(
+    name="StormGain", domain="stormgain.com", bad_type="криптовалютная биржа"
+)
+
+BadSite.objects.create(
+    name="Lift-me (Живая Очередь)", domain="lift-me.ru", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Sincere System Group",
+    domain="s-group.io",
+    bad_type="инвестиции в токен S-WALLET",
+)
+
+BadSite.objects.create(
+    name="EtherConnect", domain="etherconnect.co", bad_type="инвестиции в хайпы"
+)
+
+BadSite.objects.create(
+    name="Tores Coin", domain="toresbusiness.com", bad_type="инвестиции в стейкинг"
+)
+
+BadSite.objects.create(
+    name="Rcoin", domain="rcoin.bet", bad_type="инвестиции в стейкинг"
+)
+
+BadSite.objects.create(
+    name="IT Smart Money", domain="itsm-online.ru", bad_type="хайп проект"
+)
+
+BadSite.objects.create(
+    name="X-Profit", domain="x-profits.ru", bad_type="финансовая пирамида"
+)
+
+BadSite.objects.create(
+    name="Spacebot", domain="spacebot.com", bad_type="стейкинг, система обучения"
+)
+
+BadSite.objects.create(
+    name="Kripto Future",
+    domain="kriptofuture.com",
+    bad_type="хайп проект в сфере трейдинга и майнинга",
+)
+
+BadSite.objects.create(name="Torexo", domain="torexo.com", bad_type="хайп проект")
+
+BadSite.objects.create(
+    name="DAO Consensus", domain="daoconsensus.com", bad_type="инвестиции в блокчейн"
+)
+
+BadSite.objects.create(name="Senexa", domain="senexa.biz", bad_type="")
+
+BadSite.objects.create(
+    name="Grame", domain="grame.info", bad_type="криптовалюта Grame , стейкинг"
+)
+
+BadSite.objects.create(
+    name="Dividendo",
+    domain="dividendo.pro",
+    bad_type="финансовая пирамида с пассивным доходом",
+)
+
+BadSite.objects.create(name="Antoreum", domain="antoreum.com", bad_type="хайп проект")
+
+BadSite.objects.create(
+    name="Goku Market",
+    domain="gokumarket.com",
+    bad_type="криптовалютная биржа с пассивным доходом",
+)
